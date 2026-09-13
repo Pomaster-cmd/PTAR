54a47bdf78d8a5aa36c3ad8eaf85451703db90c4
Pomaster-cmd <contact.pomaster@gmail.com>
2026-09-13 22:21:58 +0200

    lab: split RC41 target classifier interface

---
+++ lab/raster/rc41/ptar_rc41_target_classifier.h
@@ -0,0 +1,26 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <d3d11.h>
+
+namespace ptar_rc41 {
+
+class TargetClassifier {
+public:
+    TargetClassifier() noexcept;
+    ~TargetClassifier();
+    TargetClassifier(const TargetClassifier&) = delete;
+    TargetClassifier& operator=(const TargetClassifier&) = delete;
+
+    bool set_primary_resource(ID3D11Resource* resource) noexcept;
+    void clear_primary() noexcept;
+    bool is_primary(ID3D11RenderTargetView* rtv) const noexcept;
+    bool any_primary(UINT count, ID3D11RenderTargetView* const* rtvs) const noexcept;
+
+private:
+    mutable SRWLOCK lock_;
+    IUnknown* primaryIdentity_;
+};
+
+} // namespace ptar_rc41
