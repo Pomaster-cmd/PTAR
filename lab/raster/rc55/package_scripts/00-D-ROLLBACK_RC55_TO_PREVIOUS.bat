12d45ff03be945c8f82fc2a664d3f2cd26f7d1d0
Pomaster-cmd <contact.pomaster@gmail.com>
2026-09-15 09:38:00 +0200

    lab(rc55): add rollback launcher

---
+++ lab/raster/rc55/package_scripts/00-D-ROLLBACK_RC55_TO_PREVIOUS.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal EnableExtensions
+cd /d "%~dp0"
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0RC55_BOUND_PHYSICAL_FIX\rollback.ps1"
+set "RC=%ERRORLEVEL%"
+echo.
+pause
+exit /b %RC%
